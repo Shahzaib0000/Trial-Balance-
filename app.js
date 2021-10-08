22fcac15aa864044c7c032f7e6a67aee8e07d637
Shahzaib Awan <52386022+Shahzaib0000@users.noreply.github.com>
2021-10-09 02:03:35 +0500

    Add files via upload
    css and new files are added

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ function adjustments() {
     // create table head
     var tablebody = document.createElement("tbody")
     var tr = document.createElement("tr")
+    tr.setAttribute("id" ,"tablerow")
     var td1 = document.createElement("td");
     td1Text = date.value;
     var td2 = document.createElement("td");
     td2Text = accountDetails.value;
     var td3 = document.createElement("td");
     var td3Text;
-    // td3Text = "";
     var td4 = document.createElement("td");
-    // td4Text = "";
+
 
     td1.append(td1Text)
     td2.append(td2Text)
@@ -30,29 +30,31 @@ function adjustments() {
         td3.append(td3Text);
         tr.appendChild(td3)
 
-        // var td4Text;
-        // td4Text == "";
-        // tr.append(td4Text)
-        // tr.appendChild(td4)
+        var emptyell = document.createElement("td");
+        emptyCellText = "";
+        emptyell.append(emptyCellText);
+        tr.appendChild(emptyell);
     }
+
     else if (entryNature.value === "Credit" || entryNature.value === "credit") {
-        
-        td3Text == "";
+        var emptyell = document.createElement("td");
+        emptyCellText = "";
+        emptyell.append(emptyCellText);
+        tr.appendChild(emptyell);
+
         var td4Text;
-        td4Text == amount.value;
+        td4Text = amount.value;
         td4.append(td4Text);
         tr.appendChild(td4)
     }
     else {
         alert("Account should be Debit or Credit")
     }
-
-
-
     tablebody.appendChild(tr);
-
-    // tablebody.appendChild(tr);
-    // myTable.appendChild(tablehead)
     myTable.appendChild(tablebody)
 
+    // creating trial balance making button
+}
+function trialbalance() {
+        window.location = "trialbalance/trial.html";
 }
